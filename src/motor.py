5e9a666e6a9d24034f94525c76e6dcf0adcb24f3
FilipGrela <fgrela09@gmail.com>
2022-11-05 16:58:32 +0000

    Add drive future

--- src/motor.py
+++ src/motor.py
@@ -4,55 +4,56 @@ import RPi.GPIO as GPIO
 
 
 class Motor:
-    def __init__(self, pinFwd, pinRev, frequency=Constants.PWM.FREQUENCY, maxSpeed=100):
+    def __init__(self, reverse: bool, pinFwdPwm: int, pinRev: int, frequency=Constants.PWM.FREQUENCY, maxSpeed=100):
         GPIO.setmode(GPIO.BCM)
         GPIO.setwarnings(False)
-        GPIO.setup(pinFwd,  GPIO.OUT)
+        GPIO.setup(pinFwdPwm,  GPIO.OUT)
         GPIO.setup(pinRev, GPIO.OUT)
+
+        self.pinRev = pinRev
+        self._reverse = reverse
 
         self._frequency = frequency
         self._maxSpeed = maxSpeed
-        self._pwmFwd = GPIO.PWM(pinFwd, frequency)
-        self._pwmRev = GPIO.PWM(pinRev, frequency)
+        self._pwm = GPIO.PWM(pinFwdPwm, frequency)
         self.stop()
 
     def stop(self):
-
-        #self._pwmFwd.start(0)
-        #self._pwmRev.start(0)
-
-        self._pwmFwd.stop()
-        self._pwmRev.stop()
+        self._pwm.stop()
 
     def run(self, speed=None):
+
         if speed == None:
             speed = self._maxSpeed
 
         if speed > 100:
             speed = 100
-        if speed < -100:
+        elif speed < -100:
             speed = -100
 
         print(speed)
 
+        if self._reverse:
+            speed = speed*-1
+        print(self._reverse)
+
         if speed < 0:
-            self._pwmRev.start(speed * -1)
-            self._pwmRev.start(0)
+            GPIO.output(self.pinRev, GPIO.HIGH)
         else:
-            self._pwmFwd.start(speed)
-            self._pwmRev.start(0)
+            GPIO.output(self.pinRev, GPIO.LOW)
+
+        self._pwm.start(abs(speed))
 
     def driveBackwards(self, motorPin1, motorPin2):
         GPIO.setup(motorPin1,  GPIO.OUT)
         GPIO.setup(motorPin2,  GPIO.OUT)
 
-        self.output(motorPin1, true)
-        self.output(motorPin2, true)
+        self.output(motorPin1, True)
+        self.output(motorPin2, True)
 
     def driveForward(self, motorPin1, motorPin2):
         GPIO.setup(motorPin1,  GPIO.OUT)
         GPIO.setup(motorPin2,  GPIO.OUT)
 
-        self.output(motorPin1, true)
-        self.output(motorPin2, false)
-        
+        self.output(motorPin1, True)
+        self.output(motorPin2, False)
--- src/tank_drive.py
+++ src/tank_drive.py
@@ -2,13 +2,22 @@ from motor import Motor
 
 
 class TankDrive:
-    def __init__(self, left_motor: Motor, righ_motor: Motor):
-        self._left_motor = left_motor
-        self._right_motor = righ_motor
+    def __init__(self,
+                 front_left_motor: Motor, front_righ_motor: Motor,
+                 back_left_motor: Motor, back_righ_motor: Motor):
+
+        self._front_left_motor = front_left_motor
+        self._front_right_motor = front_righ_motor
+
+        self._back_left_motor = back_left_motor
+        self._back_right_motor = back_righ_motor
 
     def drive(self, speed, turn):
-        left_motor_speed = speed + turn * 0.3
-        right_motor_speed = speed - turn * 0.3
+        left_speed = speed + turn * 0.3
+        right_speed = speed - turn * 0.3
 
-        self._left_motor.run(left_motor_speed)
-        self._right_motor.run(right_motor_speed)
+        self._front_left_motor.run(left_speed)
+        self._front_right_motor.run(right_speed)
+
+        self._back_left_motor.run(left_speed)
+        self._back_right_motor.run(right_speed)
