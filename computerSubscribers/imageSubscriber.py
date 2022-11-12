c5d3c0c865388ce0df7fbad9284b7fe7c6913cdb
MrSoilder23 <72629981+MrSoilder23@users.noreply.github.com>
2022-11-12 23:14:35 +0100

    add imageSubscriber

---
+++ computerSubscribers/imageSubscriber.py
@@ -0,0 +1,39 @@
+import rclpy
+from rclpy.node import Node
+
+from std_msgs.msg import String
+from cv_bridge import CvBridge
+import cv2
+
+class imageSubscriber(Node):
+    def __init__(self):
+        super().__init__('image_subscriber')
+        self.subscription = self.create_subscription(
+            Image,
+            'video_frames',
+            self.listener_callback,
+            10)
+        self.subscription
+
+        self.br = CvBridge()
+
+    def listener_callback(self, data):
+        self.get_logger().info('Receiving video frame')
+
+        current_frame = self.br.imgmsg_to_cv2(data)
+
+        cv2.imgshow("camera", current_frame)
+
+        cv2.waitKey(1)
+
+    def main(args=None):
+        rclpy.init(args=args)
+
+        image_subscriber = imageSubscriber()
+
+        rclpy.spin(image_subscriber)
+
+        rclpy.shutdown()
+
+    if __name__ == '__main__':
+        main()
